cd079e4b5093e78796bccf11a71d46620a5657df
Carl Allen <carl@allenofmn.com>
2017-07-27 17:26:42 -0500

    Better forced installation script

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 from modules import app, cbpi
 try:
   from flask_basicauth import BasicAuth
-  basic_auth = BasicAuth(app)
 except:
-  import thread, os
-  thread.start_new_thread(os.system,('pip install Flask-BasicAuth',))
-  cbpi.notify("HTTPAuth Error", "Flask-BasicAuth was not insalled. Restart to activate HTTPAuth", type="danger", timeout=10000)
+  import os
+  os.system('pip install Flask-BasicAuth')
+  from flask_basicauth import BasicAuth
 
+basic_auth = BasicAuth(app)
 def auth_password():
   password = cbpi.get_config_parameter('auth_password', None)
   if password is None:
