ca7a44e4f9d9f70f7d89ca3fe88c60cec0d04e15
Carl Allen <carl@allenofmn.com>
2017-07-27 17:11:21 -0500

    auto-install Flask-BasicAuth

--- __init__.py
+++ __init__.py
@@ -1,5 +1,12 @@
 from modules import app, cbpi
-from flask_basicauth import BasicAuth
+try:
+  from flask_basicauth import BasicAuth
+  basic_auth = BasicAuth(app)
+except:
+  improt os
+  os.system("pip install Flask-BasicAuth")
+  cbpi.notify("HTTPAuth Error", "Flask-BasicAuth was not insalled. Restart to activate HTTPAuth", type="danger", timeout=10000)
+
 def auth_password():
   password = cbpi.get_config_parameter('auth_password', None)
   if password is None:
@@ -7,8 +14,6 @@ def auth_password():
     return "password"
   else:
     return password
-
-basic_auth = BasicAuth(app)
 
 app.config['BASIC_AUTH_USERNAME'] = 'admin'
 app.config['BASIC_AUTH_PASSWORD'] = auth_password()
